e4681bee6fbca6c482d4fc50fa68844798bbea10
Théo GUEURET <tgueuret@live.fr>
2022-12-04 12:23:17 +0100

    Grid.py

---
+++ main.py
@@ -0,0 +1,13 @@
+<<<<<<< Updated upstream
+=======
+import numpy as np
+from Grid import Grid
+
+
+shape = (4, 4)
+model = np.arange(shape[0] * shape[1]).reshape(shape)
+
+grid = Grid(shape[0], shape[1])
+grid.initialization(2)
+print(grid)
+>>>>>>> Stashed changes
