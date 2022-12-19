ab413051300a085a453245766e7055dc57d3fe1c
Théo <tgueuret@live.fr>
2022-12-19 23:01:09 +0100

    implemented policies

--- State.py
+++ State.py
@@ -2,29 +2,36 @@ import numpy as np
 
 class State:
 
-    def __init__(self) -> None:
-        self.grid = np.zeros(shape=(4,4))
-    
-    def set(self, model) -> None:
-        self.grid = model
+    #-------------------------------------------------------------------------------------------
+    #------------------------STATE FUNCTIONS----------------------------------------------------  
 
-    #------------------------------------------------------------------------
-    #------------------------STATE FUNCTIONS---------------------------------    
-
-    def get_successors(self) -> None:
+    def get_successors(self, grid) -> list:
         succesors = []
-        right = self.roll_right()
-        left = self.roll_left()
-        up = self.roll_up()
-        down = self.roll_down()
-        succesors.extend([right, left, up, down])
+        right = self.roll_right(grid)
+        print(right)
+        succesors.extend(self.all_posibilities(right))
+        left = self.roll_left(grid)
+        succesors.extend(self.all_posibilities(left))
+        up = self.roll_up(grid)
+        succesors.extend(self.all_posibilities(up))
+        down = self.roll_down(grid)
+        succesors.extend(self.all_posibilities(down))
         return succesors
+
+    # score of a grid
+    def policies(self, grid) -> int:
+        return np.sum(grid)
+
+    def esperance_successors(self, successors) -> int:
+        results = np.empty(shape=len(successors))
+        for index, successor in enumerate(successors):
+            results[index] = self.policies(successor)
+        return np.mean(results)
+
+        
     
-
-
-
-    #------------------------------------------------------------------------
-    #------------------------ENGINE FUNCTIONS--------------------------------
+    #-------------------------------------------------------------------------------------------
+    #------------------------ENGINE FUNCTIONS---------------------------------------------------
 
     def perform_simplification(self, row) -> np.array:
         if np.sum(row) == 0: return row
@@ -38,34 +45,43 @@ class State:
         row = np.append(row, np.zeros(shape=(initial_shape-row.shape[0])))
         return row
 
-    def roll_left(self) -> np.array:
-        resulted_array = np.array(self.grid) # a copy but faster
-        for index, row in enumerate(self.grid):
-            resulted_array[index] = self.perform_simplification(row)
-        return resulted_array
+    def roll_left(self, grid) -> np.array:
+        resulted_grid = np.array(grid) # a copy but faster
+        for index, row in enumerate(resulted_grid):
+            resulted_grid[index] = self.perform_simplification(row)
+        return resulted_grid
 
-    def roll_right(self) -> np.array:
-        resulted_array = np.array(self.grid) # a copy but faster
-        for index, row in enumerate(self.grid):
-            resulted_array[index] = np.flip(self.perform_simplification(np.flip(row)))
-        return resulted_array
+    def roll_right(self, grid) -> np.array:
+        resulted_grid = np.array(grid) # a copy but faster
+        for index, row in enumerate(resulted_grid):
+            resulted_grid[index] = np.flip(self.perform_simplification(np.flip(row)))
+        return resulted_grid
 
-    def roll_up(self) -> np.array:
-        resulted_array = np.array(self.grid) # a copy but faster
-        for index in range(self.grid.shape[1]):
-            col = self.grid[:, index]
-            resulted_array[:, index] = self.perform_simplification(col)
-        return resulted_array
+    def roll_up(self, grid) -> np.array:
+        resulted_grid = np.array(grid) # a copy but faster
+        for index in range(resulted_grid.shape[1]):
+            col = resulted_grid[:, index]
+            resulted_grid[:, index] = self.perform_simplification(col)
+        return resulted_grid
 
-    def roll_down(self) -> np.array:
-        resulted_array = np.array(self.grid) # a copy but faster
-        for index in range(self.grid.shape[1]):
-            col = self.grid[:, index]
-            resulted_array[:, index] = np.flip(self.perform_simplification(np.flip(col)))
-        return resulted_array    
+    def roll_down(self, grid) -> np.array:
+        resulted_grid = np.array(grid) # a copy but faster
+        for index in range(resulted_grid.shape[1]):
+            col = resulted_grid[:, index]
+            resulted_grid[:, index] = np.flip(self.perform_simplification(np.flip(col)))
+        return resulted_grid    
 
     # after a mouvement is made, we attribute a value (2 or 4) to a random empty cell
     # this list all of the possibilities
-    def all_posibilities(self) -> np.array:
-
-        pass
+    def all_posibilities(self, grid) -> np.array:
+        possibilities = []
+        memory = np.array(grid)
+        for index_y in range(memory.shape[0]):
+            for index_x in range(memory.shape[1]):
+                if memory[index_y, index_x] == 0:
+                    memory[index_y, index_x] = 2
+                    possibilities.append(np.array(memory))
+                    memory[index_y, index_x] = 4
+                    possibilities.append(np.array(memory))
+                    memory[index_y, index_x] = 0
+        return possibilities
--- main_ai.py
+++ main_ai.py
@@ -1,7 +1,11 @@
-from Test import Test
 from State import State
 import numpy as np
 
 game = State()
-game.set(np.array([[0,0,0,0], [0,0,1,0], [0,1,0,0], [0,0,0,0]]))
-print(game.get_successors())
+state = np.array([[0,0,0,0], [0,0,1,0], [0,1,0,0], [0,0,0,0]])
+successors = game.get_successors(state)
+
+for index, successor in enumerate(successors):
+    print("child n°:", index, "    score: ", game.policies(successor))
+
+print("final_res = ", game.esperance_successors(successors))
