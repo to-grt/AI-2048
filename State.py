4a48402d6c4d8cba01824d0b8dfefdf915ac2040
Théo <tgueuret@live.fr>
2022-12-21 19:18:38 +0100

    game loop for ai

--- State.py
+++ State.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 class State:
 
@@ -38,10 +39,90 @@ class State:
 
     # score of a grid
     def policies(self, grid) -> int:
-        return np.sum(grid)
+        score = 0
+        for row in grid:
+            for cell in row:
+                if cell != 0:
+                    score += np.log(cell)
+        return score
 
     #-------------------------------------------------------------------------------------------
     #------------------------ENGINE FUNCTIONS---------------------------------------------------
+
+    def clear(self): os.system('cls')
+
+    def game_loop(self, grid) -> None:
+
+        command = ""
+        while not self.is_game_over(grid) and not self.is_win(grid) and command != "exit":
+
+            self.clear()
+            print("----------------------\n\n",grid, "\n\n")
+            command = input("What do you want to do?\n>> ")
+            if command == "up" or command == "z":
+                print("up")
+                up = self.roll_up(grid)
+                if (up != grid).any(): grid = self.set_random_cells(up, 1)
+            elif command == "down" or command == "s":
+                print("down")
+                down = self.roll_down(grid)
+                if (down != grid).any(): grid = self.set_random_cells(down, 1)
+            elif command == "left" or command == "q":
+                print("left")
+                left = self.roll_left(grid)
+                if (left != grid).any(): grid = self.set_random_cells(left, 1)
+            elif command == "right" or command == "d":
+                print("right")
+                right = self.roll_right(grid)
+                if (right != grid).any(): grid = self.set_random_cells(right, 1)
+            elif command == "exit": pass
+            else: input("Command not recognized, press ay key to continue...\n>> ")
+            
+            if self.is_game_over(grid):
+                self.clear()
+                print("----------------------\n\n",grid, "\n\n")
+                print("You have lost the game, the higher cell you reached was: ", np.max(grid))
+                input("Press any key to continue...\n>> ")
+            
+            if self.is_win(grid):
+                self.clear()
+                print("----------------------\n\n",grid, "\n\n")
+                print("You won the game !! Good job")
+                input("Press any key to continue...\n>> ")
+            
+            if command == "exit":
+                self.clear()
+                print("----------------------\n\n",grid, "\n\n")
+                print("You decided to exit the game, we hope to see you soon")
+                input("Press any key to continue...\n>> ")
+                pass
+
+    def set_random_cells(self, grid, nb_cells) -> np.array:
+        resulted_grid = np.array(grid)
+        for _ in range(nb_cells):
+            index_row = np.random.randint(0, resulted_grid.shape[0])
+            index_column = np.random.randint(0, resulted_grid.shape[1])
+            if resulted_grid[index_row, index_column] == 0:
+                random_value = np.random.randint(0, 10)
+                resulted_grid[index_row, index_column] = 2 if random_value <= 8 else 4
+            else: 
+                return self.set_random_cells(grid, 1)
+        return resulted_grid
+
+    def is_game_over(self, grid) -> bool:     #TODO probably optimizable
+        down = self.roll_down(grid)
+        if not np.array_equal(grid, down): return False
+        up = self.roll_up(grid)
+        if not np.array_equal(grid, up): return False
+        left = self.roll_left(grid)
+        if not np.array_equal(grid, left): return False
+        right = self.roll_right(grid)
+        if not np.array_equal(grid, right): return False    
+        return True    
+
+    def is_win(self, grid) -> bool:
+        if np.max(grid) >= 2048: return True
+        return False     
 
     def perform_simplification(self, row) -> np.array:
         if np.sum(row) == 0: return row
--- main_ai.py
+++ main_ai.py
@@ -1,7 +1,16 @@
 from State import State
 import numpy as np
 
+def game_loop():
+    pass
+
 game = State()
-state = np.array([[0,0,0,0], [0,0,1,0], [0,1,0,0], [0,0,0,0]])
+state = np.array([[0,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
+"""
 esp = game.get_esperances(state)
 print("esperances: ", esp)
+"""
+
+game.game_loop(state)
+
+
