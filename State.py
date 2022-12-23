b721fb60c4f31a7389a0229f92bd23bec7e48706
Théo <tgueuret@live.fr>
2022-12-23 15:20:26 +0100

    distance and sum correction

--- State.py
+++ State.py
@@ -6,6 +6,7 @@ class State:
 
     def __init__(self) -> None:
         self.max_distances = 0
+        self.sum_max = 0
 
     #-------------------------------------------------------------------------------------------
     #------------------------USUAL FUNCTIONS----------------------------------------------------  
@@ -74,9 +75,12 @@ class State:
     # score of a grid, where the policies applied.
     def policies(self, grid) -> int:
         score_nb_empty_cells = self.min_max_norm(grid[grid==0].shape[0], 0, 16)
-        score_sum_grid = self.min_max_norm(np.sum(grid), 0, np.max(grid))
+        sum_grid = np.sum(grid)
+        if sum_grid > self.sum_max: self.sum_max = sum_grid
+        score_sum_grid = self.min_max_norm(sum_grid, 0, self.sum_max)
         arg_max = np.unravel_index(np.argmax(grid, axis=None), grid.shape)
-        score_distance_corner = self.min_max_norm(np.sqrt((arg_max[0] - 3)**2 + (arg_max[1] - 0)**2), 0, np.sqrt(18))
+        distance_closest_corner = np.min(np.array([np.sqrt((arg_max[0] - 0)**2 + (arg_max[1] - 0)**2), np.sqrt((arg_max[0] - 3)**2 + (arg_max[1] - 3)**2), np.sqrt((arg_max[0] - 3)**2 + (arg_max[1] - 0)**2), np.sqrt((arg_max[0] - 3)**2 + (arg_max[1] - 3)**2)]))
+        score_distance_corner = 1-self.min_max_norm(distance_closest_corner, 0, np.sqrt(18))
         sum_distance = 0
         for index_y, row in enumerate(grid):
             for index_x, _ in enumerate(row):
@@ -85,8 +89,8 @@ class State:
                 if index_y != 0: sum_distance += np.abs(grid[index_y-1, index_x] - grid[index_y, index_x])
                 if index_y != grid.shape[0]-1: sum_distance += np.abs(grid[index_y+1, index_x] - grid[index_y, index_x])
         if sum_distance > self.max_distances: self.max_distances = sum_distance
-        score_sum_distance = self.min_max_norm(sum_distance, 0, self.max_distances)
-        score = score_nb_empty_cells + score_sum_grid + (1-score_distance_corner) +  (1-score_sum_distance)
+        score_sum_distance = 1-self.min_max_norm(sum_distance, 0, self.max_distances)
+        score = score_nb_empty_cells + score_sum_grid + score_distance_corner +  score_sum_distance
         return score
 
     def min_max_norm(self, value, min, max) -> float:
--- main_ai.py
+++ main_ai.py
@@ -5,6 +5,3 @@ game = State()
 state = game.set_random_cells(np.array([[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]), 2)
 
 game.ai_loop (state)
-
-
- 
