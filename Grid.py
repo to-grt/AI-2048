67af1bd420ef477b799e202a6a7dfa5419883ad8
RomainD_ <romain.Dreuilhet@protonmail.com>
2022-12-30 11:11:33 +0100

    Parse init grid value into int. And modify color dictionary.

--- Grid.py
+++ Grid.py
@@ -7,31 +7,31 @@ CELL_PADDING = 10
 BACKGROUND_COLOR = '#92877d'
 EMPTY_CELL_COLOR = '#9e948a'
 CELL_BACKGROUND_COLOR_DICT = {
-    '2.0': '#eee4da',
-    '4.0': '#ede0c8',
-    '8.0': '#f2b179',
-    '16.0': '#f59563',
-    '32.0': '#f67c5f',
-    '64.0': '#f65e3b',
-    '128.0': '#edcf72',
-    '256.0': '#edcc61',
-    '512.0': '#edc850',
-    '1024.0': '#edc53f',
-    '2048.0': '#edc22e',
+    '2': '#eee4da',
+    '4': '#ede0c8',
+    '8': '#f2b179',
+    '16': '#f59563',
+    '32': '#f67c5f',
+    '64': '#f65e3b',
+    '128': '#edcf72',
+    '256': '#edcc61',
+    '512': '#edc850',
+    '1024': '#edc53f',
+    '2048': '#edc22e',
     'beyond': '#3c3a32'
 }
 CELL_COLOR_DICT = {
-        '2.0': '#776e65',
-        '4.0': '#776e65',
-        '8.0': '#f9f6f2',
-        '16.0': '#f9f6f2',
-        '32.0': '#f9f6f2',
-        '64.0': '#f9f6f2',
-        '128.0': '#f9f6f2',
-        '256.0': '#f9f6f2',
-        '512.0': '#f9f6f2',
-        '1024.0': '#f9f6f2',
-        '2048.0': '#f9f6f2',
+        '2': '#776e65',
+        '4': '#776e65',
+        '8': '#f9f6f2',
+        '16': '#f9f6f2',
+        '32': '#f9f6f2',
+        '64': '#f9f6f2',
+        '128': '#f9f6f2',
+        '256': '#f9f6f2',
+        '512': '#f9f6f2',
+        '1024': '#f9f6f2',
+        '2048': '#f9f6f2',
         'beyond': '#f9f6f2'
 }
 
@@ -46,7 +46,7 @@ class Grid:
     def __init__(self, n, prints=False):
         self.prints = prints
         self.size = n
-        self.grid = np.zeros((n, n))
+        self.grid = np.zeros((n, n), dtype=int)
         self.current_score = 0
 
         self.root = tk.Tk()
